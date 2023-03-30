ea4b3cb640537de71aaf223b13e1663b6199088b
midlajt <muhammedmidlaj236@gmail.com>
2023-03-30 12:34:54 +0530

    product-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
